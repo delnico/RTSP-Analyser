f8e2ad9e96d28d697137a9e277aff5f4fa108de8
delnico <deleuzenico.nd@gmail.com>
2026-03-01 21:25:15 +0100

    streamreceiver ambigious namespace

--- RtspAnalyser.Streamers/src/StreamReceiver.cpp
+++ RtspAnalyser.Streamers/src/StreamReceiver.cpp
@@ -14,98 +14,97 @@
 #include "DelNico/RtspAnalyser/Libs/Stream.h"
 #include "DelNico/RtspAnalyser/Analyser/IAnalyser.h"
 
-using namespace DelNico::RtspAnalyser::Streamers;
-
-
-StreamReceiver::StreamReceiver(
-    boost::asio::io_service & io_service,
-    const Libs::Stream & stream,
-    std::deque<cv::Mat> & frames
-) :
-    isEnabled(false),
-    timer(boost::asio::deadline_timer(io_service, boost::posix_time::microsec(stream.frequency.count()))),
-    stream(stream),
-    cap(),
-    frames(frames)
-{
-}
-
-StreamReceiver::~StreamReceiver()
-{
-    stop();
-}
-
-void StreamReceiver::start(boost::asio::io_service & io_service)
-{
-    isEnabled.store(true);
-    cap.open(stream.url, cv::CAP_FFMPEG);
-    if(!cap.isOpened())
-        throw std::runtime_error("Failed to open stream");
-    timer = boost::asio::deadline_timer(io_service, boost::posix_time::microsec(stream.frequency.count()));
-    timer.async_wait(boost::bind(&StreamReceiver::run, this));
-}
-
-void StreamReceiver::stop()
-{
-    if(isEnabled.load())
+namespace DelNico::RtspAnalyser::Streamers {
+    StreamReceiver::StreamReceiver(
+        boost::asio::io_service & io_service,
+        const Libs::Stream & stream,
+        std::deque<cv::Mat> & frames
+    ) :
+        isEnabled(false),
+        timer(boost::asio::deadline_timer(io_service, boost::posix_time::microsec(stream.frequency.count()))),
+        stream(stream),
+        cap(),
+        frames(frames)
     {
-        isEnabled.store(false);
-        timer.cancel();
     }
-}
 
-void StreamReceiver::subscribe(Analyser::IAnalyser * analyser)
-{
-    listener = analyser;
-}
-
-void StreamReceiver::unsubscribe(Analyser::IAnalyser * analyser)
-{
-    listener = nullptr;
-}
-
-void StreamReceiver::watchdog()
-{
-    if(queueSize() > 3) {
-        goToLive();
-    }
-}
-
-int64_t StreamReceiver::queueSize() const
-{
-    return frames.size();
-}
-
-void StreamReceiver::goToLive()
-{
-    // let clear frames queue but keep the last frame (and one before, for secure queue)
-    while(frames.size() > 2)
+    StreamReceiver::~StreamReceiver()
     {
-        frames.pop_front();
+        stop();
     }
-}
 
-void StreamReceiver::run()
-{
-    cv::Mat frame;
-    std::chrono::duration<double> elapsed(0);
-    while (isEnabled)
+    void StreamReceiver::start(boost::asio::io_service & io_service)
     {
-        auto start = std::chrono::steady_clock::now();
-        cap >> frame;
-
-        if(! frame.empty() && frame.size().width > 0 && frame.size().height > 0)
-        {
-            if(listener != nullptr)
-            {
-                frames.push_back(frame);
-                listener->notify();
-            }
-        }
-        auto end = std::chrono::steady_clock::now();
-        elapsed = end - start;
-        auto sleep_duration = std::chrono::microseconds((int64_t) (stream.frequency.count() - (elapsed.count() * 1000)));
-        timer.expires_at(timer.expires_at() + boost::posix_time::microsec(sleep_duration.count()));
+        isEnabled.store(true);
+        cap.open(stream.url, cv::CAP_FFMPEG);
+        if(!cap.isOpened())
+            throw std::runtime_error("Failed to open stream");
+        timer = boost::asio::deadline_timer(io_service, boost::posix_time::microsec(stream.frequency.count()));
         timer.async_wait(boost::bind(&StreamReceiver::run, this));
+    }
+
+    void StreamReceiver::stop()
+    {
+        if(isEnabled.load())
+        {
+            isEnabled.store(false);
+            timer.cancel();
+        }
+    }
+
+    void StreamReceiver::subscribe(Analyser::IAnalyser * analyser)
+    {
+        listener = analyser;
+    }
+
+    void StreamReceiver::unsubscribe(Analyser::IAnalyser * analyser)
+    {
+        listener = nullptr;
+    }
+
+    void StreamReceiver::watchdog()
+    {
+        if(queueSize() > 3) {
+            goToLive();
+        }
+    }
+
+    int64_t StreamReceiver::queueSize() const
+    {
+        return frames.size();
+    }
+
+    void StreamReceiver::goToLive()
+    {
+        // let clear frames queue but keep the last frame (and one before, for secure queue)
+        while(frames.size() > 2)
+        {
+            frames.pop_front();
+        }
+    }
+
+    void StreamReceiver::run()
+    {
+        cv::Mat frame;
+        std::chrono::duration<double> elapsed(0);
+        while (isEnabled)
+        {
+            auto start = std::chrono::steady_clock::now();
+            cap >> frame;
+
+            if(! frame.empty() && frame.size().width > 0 && frame.size().height > 0)
+            {
+                if(listener != nullptr)
+                {
+                    frames.push_back(frame);
+                    listener->notify();
+                }
+            }
+            auto end = std::chrono::steady_clock::now();
+            elapsed = end - start;
+            auto sleep_duration = std::chrono::microseconds((int64_t) (stream.frequency.count() - (elapsed.count() * 1000)));
+            timer.expires_at(timer.expires_at() + boost::posix_time::microsec(sleep_duration.count()));
+            timer.async_wait(boost::bind(&StreamReceiver::run, this));
+        }
     }
 }
